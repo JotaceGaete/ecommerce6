0e240d0feb3d6e9f7d3f369a0f524deb58d4cdf6
JotaceGaete <jotacegaete@gmail.com>
2025-05-19 15:49:37 -0400

    refactor: eliminar campo teléfono del registro

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -63,13 +63,6 @@ export default function Register() {
     rePassword: Yup.string()
       .required('Confirm password is required')
       .oneOf([Yup.ref('password')], 'Passwords do not match'),
-
-    phone: Yup.string()
-      .required('Phone number is required')
-      .matches(
-        /^9[0-9]{8}$/,
-        'Phone number must be in the format 9XXXXXXXX'
-      ),
   });
 
   const formik = useFormik({
@@ -78,7 +71,6 @@ export default function Register() {
       email: '',
       password: '',
       rePassword: '',
-      phone: '',
     },
     onSubmit: handleRegister,
     validationSchema: validate,
@@ -189,29 +181,6 @@ export default function Register() {
             {formik.errors.rePassword && formik.touched.rePassword && (
               <span className="text-red-600 font-light text-sm">
                 {formik.errors.rePassword}
-              </span>
-            )}
-          </div>
-          <div className="relative z-0 w-full mb-5 group">
-            <input
-              type="tel"
-              name="phone"
-              id="phone"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.phone}
-              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-green-600 peer"
-              placeholder="9XXXXXXXX"
-            />
-            <label
-              htmlFor="phone"
-              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Phone number (123-456-7890)
-            </label>
-            {formik.errors.phone && formik.touched.phone && (
-              <span className="text-red-600 font-light text-sm">
-                {formik.errors.phone}
               </span>
             )}
           </div>
